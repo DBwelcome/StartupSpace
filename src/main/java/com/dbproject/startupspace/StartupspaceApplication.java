ee63f59e250517e23c24ecb5c1b910df769e9e90
김영진 <youngjin033@ajou.ac.kr>
2020-11-21 16:54:27 +0900

    initial commit

---
+++ src/main/java/com/dbproject/startupspace/StartupspaceApplication.java
@@ -0,0 +1,13 @@
+package com.dbproject.startupspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbproject/startupspace/StartupspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbproject.startupspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
