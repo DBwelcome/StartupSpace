f37abe86878d49f5d0925e82de9cb250ea316373
김영진 <youngjin033@ajou.ac.kr>
2020-11-24 18:00:47 +0900

    entity 추가 작성

--- src/main/java/com/dbproject/startupspace/domain/entity/MetropolitanCenterEntity.java
+++ src/main/java/com/dbproject/startupspace/domain/entity/MetropolitanCenterEntity.java
@@ -1,4 +1,79 @@
 package com.dbproject.startupspace.domain.entity;
 
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name="metropolitancenter")
 public class MetropolitanCenterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int centerId;
+
+    @Column(length = 50, nullable = false)
+    private String centerNum;
+
+    @Column(length = 50, nullable = false)
+    private String centerType;
+
+    @Column(length = 50, nullable = false)
+    private String buildingName;
+
+    @Column(nullable = false)
+    private int postNum;
+
+    @Column(length = 100,nullable = false)
+    private String address;
+
+    @Column(length = 12)
+    private String telephone;
+
+    @Column(length = 12)
+    private String fax;
+
+    @Column(length = 100)
+    private String email;
+
+    @Column(length = 100)
+    private String homepage;
+
+    @Column(columnDefinition = "TEXT")
+    private String centerIntro;
+
+    @Column(nullable = false)
+    private int spaceCount;
+
+    @Column(length = 2)
+    private String area;
+
+    @Column(nullable = false)
+    private int target;
+
+    @Builder
+    public MetropolitanCenterEntity(int centerId, String centerNum, String centerType, String buildingName, int postNum,
+                                    String address, String telephone, String fax, String email, String homepage,
+                                    String centerIntro, int spaceCount, String area, int target){
+        this.centerId = centerId;
+        this.centerNum = centerNum;
+        this.centerType = centerType;
+        this.buildingName = buildingName;
+        this.postNum = postNum;
+        this.address = address;
+        this.telephone = telephone;
+        this.fax = fax;
+        this.email = email;
+        this.homepage = homepage;
+        this.centerIntro = centerIntro;
+        this.spaceCount = spaceCount;
+        this.area = area;
+        this.target = target;
+    }
 }
